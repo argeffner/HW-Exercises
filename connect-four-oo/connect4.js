fa1b9c678fb6630c93a592b06410d023df2e356c
Alon Rudi Geffner <alonrgeffner@alons-mbp.home>
2020-10-29 18:42:42 -0700

    connect 4 only

--- connect-four-oo/connect4.js
+++ connect-four-oo/connect4.js
@@ -6,10 +6,10 @@
  */
 // making an interchangeable background image for each reset
 
-
-
-  class Game {
+  class Game  {
     constructor(p1, p2, height = 6, width = 7) {
+      
+      // super(Rick, Morty, Jerry, Summer, Beth, MisterMeeseeks);
       this.randBack();
       this.players = [p1, p2];
       this.height = height;
@@ -33,7 +33,6 @@
                     "https://wallpapercave.com/wp/wp1822809.jpg",
                     "https://wallpapercave.com/wp/wp1822763.jpg",
                     "https://cdnb.artstation.com/p/assets/images/images/028/853/683/large/nicholas-cole-jordan-flgtz-rickmorty-panorama-watermarked.jpg?1595716690",
-                    "https://i.imgur.com/EMzXEH7.jpg",
                     "https://64.media.tumblr.com/e2aa898df2f0e45f4d0a25745153d11a/tumblr_nwr0qc8z3D1tc28zvo2_1280.png",
                     "https://64.media.tumblr.com/177a02310dd44a25c55c5e26f903a241/tumblr_nwr0qc8z3D1tc28zvo5_1280.png",
                     "https://64.media.tumblr.com/10b505fa86ab1639bcb71f6f6273d682/tumblr_nwr0qc8z3D1tc28zvo6_1280.png",
@@ -48,6 +47,10 @@
     /** makeBoard: create in-JS board structure:
      *   board = array of rows, each row is array of cells  (board[y][x])
      */
+     display(){
+       console.log(this.currPlayer, this.players[1]);
+      }
+
     makeBoard() {
       this.board = [];
       for (let y = 0; y < this.height; y++) {
@@ -109,7 +112,8 @@
     placeInTable(y, x) {
       const piece = document.createElement('div');
       piece.classList.add('piece');
-      // piece.style.backgroundColor = this.currPlayer.color;
+      piece.style.backgroundSize = "100px";
+      piece.style.backgroundPosition = "center";
       piece.style.backgroundImage = this.currPlayer.token
       piece.style.top = -50 * (y + 2);
   
@@ -119,8 +123,10 @@
   
     /** endGame: announce game end */
   
-    endGame(winMsg) {
-      alert(currPlayer.winMsg);
+    endGame(msg) {
+      setTimeout(function () {
+        alert(msg);
+    },700);
       const top = document.querySelector("#column-top");
       top.removeEventListener("click", this.handleGameClick);
     }
@@ -143,13 +149,13 @@
   
       // check for tie
       if (this.board.every(row => row.every(cell => cell))) {
-        return this.endGame('Tie!');
+        return this.endGame(' A Tie? Seriously?');
       }
   
       // check for win
       if (this.checkForWin()) {
         this.gameOver = true;
-        return this.endGame(`The ${this.currPlayer.color} player won!`);
+        return this.endGame(` ${this.currPlayer.winMsg} `);
       }
   
       // switch players
@@ -190,46 +196,70 @@
       }
     }
   }
-  
-  class Player extends Game{
-    constructor(Rick, Morty, Jerry, Summer, Beth, MisterMeeseeks) {
-       this.Rick = {
+
+
+    function character(name){
+      let char = {};
+    if(name === "Rick") {
+        char = {
       winMsg: `You're an idiot`,
       token: 'url(https://media1.giphy.com/media/IgpAALi5hEv1IFmCrZ/giphy.gif?cid=ecf05e47b7bu86i8j9xzn61ccuelek52jcyni635ygzymypg&rid=giphy.gif)'
-        };
-       this.Morty = {
-         winMsg: `I won, now it's my turn to choose an adventure`,
+        }};
+    if(name === "Morty") {
+        char = {
+         winMsg: `I won. Can we go home now?`,
          token: 'url(https://media1.giphy.com/media/W35DnRbN4oDHIAApdk/giphy.gif?cid=ecf05e47ry1yf7t25hw9nodo2z6gweig9qpyy08ris9g0tqd&rid=giphy.gif)'
-       };
-       this.Jerry = {
+        }};
+    if(name === "Jerry") {
+       char = {
          winMsg: `Look I actually won, Yay!`,
          token: 'url(https://media2.giphy.com/media/MaxUtki5xQdQLwUPW1/giphy.gif?cid=ecf05e47hq28covh9ulvo7ogxppfyq5vdas5n9wsdqt6mtym&rid=giphy.gif)'
-       };
-       this.Beth = {
+        }};
+    if(name === "Beth") {
+       char = {
         winMsg: `Of couse I won`,
         token: 'url(https://media0.giphy.com/media/UVNImrTih6ptsVK1br/giphy.gif?cid=ecf05e47sxuke0xacur3e2cigwyw7evnx7uvkhgqaoyt40pf&rid=giphy.gif)'
-      };
-       this.Summer = {
+        }};
+    if(name === "Summer") {
+       char = {
         winMsg: `Whatever`,
         token: 'url(https://media3.giphy.com/media/YMG6EIg4nkId3x2n61/giphy.gif?cid=ecf05e4765hsvx8n1kvl1jfmu5ubthq6c52gti98smfa4505&rid=giphy.gif)'
-      };
-       this.MisterMeeseeks = {
+        }};
+    if(name === "MisterMeeseeks") {
+       char = {
         winMsg: `I'm Mr. Meeseeks look at me`,
         token: 'url(https://media2.giphy.com/media/WvOjT988OZiNy/giphy.gif?cid=ecf05e474y1aqlp5r4tpqpw1m8gvdigspcmyrbvh0kgeayp4&rid=giphy.gif)'
-      };
+        }};
+
+        return char;
     }
-  }
-  document.getElementById("btn").onclick = function() { 
-    location.reload(); 
-  } 
+    
+    document.getElementById("Other").style.display = "none";
+    document.getElementById("btn").style.display = "none";
+
   document.getElementById('start-game').addEventListener('click', () => {
-    document.getElementById("p1-color").style.display = "none";
-    document.getElementById("p2-color").style.display = "none";
     document.getElementById("p1-character").style.display = "none";
     document.getElementById("p2-character").style.display = "none";
-    // let p1 = new Player(document.getElementById('p1-color').value);
-    // let p2 = new Player(document.getElementById('p2-color').value);
-    let p1 = new Player(document.getElementById('p1-character').value);
-    let p2 = new Player(document.getElementById('p2-character').value);
-    new Game(p1, p2);
+    document.getElementById("Hint").style.display = "none";
+    
+
+    let char1 = document.getElementById('p1-character').value;
+    let char2 = document.getElementById('p2-character').value;
+    let p1 = character(char1);
+    let p2 = character(char2);
+  
+    document.getElementById("btn").style.display = "block";
+    document.getElementById("Other").style.display = "block";
+
+    document.getElementById("btn").onclick = function() { 
+      location.reload(); 
+    } 
+    if (char1 === char2){
+      alert("WHO CARES, THERE ARE INFINITE DIMENSIONS")
+    }
+    const tryOne = new Game(p1, p2);
+    //the below line was for testing to see if obects were passed in
+    //tryOne.display()
   });
+  
+
